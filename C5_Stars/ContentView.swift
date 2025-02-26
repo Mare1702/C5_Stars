c0066db79e1d457b7ba2c31448718e2714fb0d16
Luis Mario Recinos Hernández <92188399+Mare1702@users.noreply.github.com>
2025-02-26 15:28:54 +0100

    Initial Commit

---
+++ C5_Stars/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  C5_Stars
+//
+//  Created by Luis Mario Recinos Hernández  on 26/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
